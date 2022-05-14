d1611b569525bd0499b8e5e1c753e7d4da28134a
okan-87 <okankaplan1907@gmail.com>
2022-05-14 16:08:42 +0200

    Add project files.

---
+++ GenericDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myDictionary<string, int> myDictionary = new myDictionary<string, int>();
+            myDictionary.Add("Python", 1991);
+            myDictionary.Add("Java", 1995);
+            myDictionary.Add("C#", 2000);
+
+            Console.WriteLine(myDictionary.Length);
+
+            for (int i = 0; i < myDictionary.Length; i++)
+            {
+                Console.WriteLine("{0} : {1}", myDictionary.Keys[i], myDictionary.Values[i]);
+            }
+        }
+    }
+}
---
+++ GenericDictionary/myDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDictionary
+{
+    class myDictionary<T, U>
+    {
+        T[] _key;
+        T[] _tempKey;
+        U[] _value;
+        U[] _tempValue;
+
+        public myDictionary()
+        {
+            _key = new T[0];
+            _value = new U[0];
+        }
+
+        public void Add(T key, U value)
+        {
+            _tempKey = _key;
+            _tempValue = _value;
+            _key = new T[_key.Length + 1];
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+            }
+            _value = new U[_value.Length + 1];
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+        public int Length
+        {
+            get
+            {
+                return _key.Length;
+            }
+        }
+        public T[] Keys
+        {
+            get
+            {
+                return _key;
+            }
+        }
+        public U[] Values
+        {
+            get
+            {
+                return _value;
+            }
+        }
+    }
+}
